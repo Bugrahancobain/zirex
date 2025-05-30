14391371beec4f12f64a4ec5d39d17e60aaddd6a
Bugrahancobain <bugrahancoban1@gmail.com>
2025-05-30 21:52:01 +0300

    Vercel Error Fixed

--- src/app/ourProducts/[id]/page.js
+++ src/app/ourProducts/[id]/page.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ProductDetailClient from "../../components/ProductDetailClient";
 import { getProductById, getOtherProducts } from "../../../../firebaseQueries";
+import { notFound } from "next/navigation";
 
 export default async function ProductDetailPage({ params }) {
     const product = await getProductById(params.id);
+
+    // Ürün bulunamadıysa 404 göster
+    if (!product) {
+        return notFound();
+    }
+
     const otherProducts = await getOtherProducts(params.id, 3);
 
-    return <ProductDetailClient product={product} otherProducts={otherProducts} />;
+    return (
+        <ProductDetailClient
+            product={product}
+            otherProducts={otherProducts || []}
+        />
+    );
 }
